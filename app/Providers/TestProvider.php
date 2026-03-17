63c5fb86a5459519bb632959a00f0c5cfaeb376c
soyeon <ryumirr1@naver.com>
2026-03-17 18:36:48 +0900

    add-default test classes

--- app/Providers/TestProvider.php
+++ app/Providers/TestProvider.php
@@ -3,24 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\TestRunService;
-class TestRunServiceProvider extends ServiceProvider
+use App\Services\TestService;
+
+class TestProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        $this->app->singleton('TestRunService', function () {
-            return new TestRunService();
+        $this->app->singleton('TestService', function () {
+            return new TestService();
         });
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
         //
