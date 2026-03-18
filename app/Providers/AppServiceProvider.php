da9425a1d4022c91946480812ba14ec79fb06fdb
soyeon <ryumirr1@naver.com>
2026-03-19 00:15:46 +0900

    fix: DI binding setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,13 @@
 
 namespace App\Providers;
 
-use App\Subscription\Application\Ports\PaymentGatewayInterface;
-use App\Subscription\Domain\Repositories\SubscriptionRepositoryInterface;
-use App\Subscription\Infrastructure\Gateways\StripeSubscriptionGateway;
-use App\Subscription\Infrastructure\Repositories\EloquentSubscriptionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(SubscriptionRepositoryInterface::class, EloquentSubscriptionRepository::class);
-        $this->app->bind(PaymentGatewayInterface::class, StripeSubscriptionGateway::class);
+        //
     }
 
     public function boot(): void
