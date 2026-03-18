20931243dacfcc1328eb303b3a36b6f4d2af5d31
soyeon <ryumirr1@naver.com>
2026-03-18 23:22:07 +0900

    Add app service provider, api route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,21 +2,20 @@
 
 namespace App\Providers;
 
+use App\Subscription\Application\Ports\PaymentGatewayInterface;
+use App\Subscription\Domain\Repositories\SubscriptionRepositoryInterface;
+use App\Subscription\Infrastructure\Gateways\StripeSubscriptionGateway;
+use App\Subscription\Infrastructure\Repositories\EloquentSubscriptionRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(SubscriptionRepositoryInterface::class, EloquentSubscriptionRepository::class);
+        $this->app->bind(PaymentGatewayInterface::class, StripeSubscriptionGateway::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
